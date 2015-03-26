d0b2b2a540c0b83725eb7459899562f9d4c2a2fc
Justak <k_justa@o2.pl>
2015-03-26 03:27:35 +0100

    Add password resets & email configuration

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,6 +1,17 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
+
+  	def setup
+    	@user       = users(:lunar)
+    	@other_user = users(:ala)
+  	end
+
+  	test "should redirect index when not logged in" do
+    	get :index
+    	assert_redirected_to login_url
+  	end
+
 	test "should get new" do
 		get :new
 		assert_response :success
